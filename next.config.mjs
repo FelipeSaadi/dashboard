fab15c7797405bc0122e9271185e3f0d6e1bb0a1
FelipeSaadi <felipesaadisiegert@gmail.com>
2025-09-03 04:02:57 -0300

    feat: add cors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ import { withNextVideo } from 'next-video/process'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://chart-wild-19023806.figma.site' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ]
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.pdf$/,
